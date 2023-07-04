9b6c656f34faeaad008406f37c490a5edf6a3ffc
dandyson <dannydyson297@gmail.com>
2023-07-04 19:22:20 +0900

    Rewrote registration function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,12 +16,27 @@ class AuthController extends Controller
         return 'hello';
     }
 
-    public function register(Request $request)
+    public function register(Request $request): JsonResponse
     {
-        $user = User::factory()->create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+
+        DB::transaction(function () use ($data) {
+            User::factory()->create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+        });
+         
+        return response()->json([
+            'type' => 'success',
+            'message' => 'New User Created Successfully',
         ]);
     }
 }
