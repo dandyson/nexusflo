aeeda25d1195f36ccd7f2fa0909db150de878f84
dandyson <dannydyson297@gmail.com>
2023-07-09 11:37:38 +0900

    Added reduced string length for Heroku DB & added proper use cases

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+        URL::forceScheme('https');
+        Schema::defaultStringLength(191);
+
     }
 }
