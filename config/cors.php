83ec7b54d6a35e754976f86c44134e4a423f7a4b
dandyson <dannydyson297@gmail.com>
2023-07-13 15:51:24 +0900

    Specifiying allowed origins (attempt to fix live 401 error)

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://obscure-depths-55000-3a15d913072b.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
