b57a30f78e7c47370a70d3635d983bea639d5846
dandyson <dannydyson297@gmail.com>
2023-08-12 17:15:48 +0900

    Added login link to verify email page

--- resources/js/views/auth/VerifyEmail.vue
+++ resources/js/views/auth/VerifyEmail.vue
@@ -98,7 +98,8 @@ const router = useRouter();
               <p class="fw-medium text-muted">
                 Welcome, please check your email for the verification link sent to you when you registered.
               </p>
-              <p>Don't have an account? <RouterLink :to="{ name: 'auth-signup3' }">Sign up</RouterLink>
+              <p>Already have an account? <RouterLink :to="{ name: 'auth-signin3' }">Login</RouterLink>
+              Or <RouterLink :to="{ name: 'auth-signup3' }">Sign up</RouterLink>
                 for a new account today!</p>
             </div>
             <!-- END Header -->
