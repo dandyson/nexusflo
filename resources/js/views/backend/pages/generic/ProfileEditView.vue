07c8da32ee2634b66828bbce2f9976140a950fab
dandyson <dannydyson297@gmail.com>
2023-11-14 11:26:03 +0000

    Swapped template and script around for consistency

--- resources/js/views/backend/pages/generic/ProfileEditView.vue
+++ resources/js/views/backend/pages/generic/ProfileEditView.vue
@@ -1,3 +1,151 @@
+<template>
+  <!-- Hero -->
+  <BaseBackground image="/assets/media/photos/photo10@2x.jpg">
+    <div class="bg-primary-dark-op">
+      <div class="content content-full text-center">
+        <div class="my-3">
+          <img v-if="authUser.avatar !== ''" :src="authUser.avatar" class="img-avatar img-avatar-thumb"
+            alt="User Avatar" />
+          <img v-else src="/assets/media/avatars/avatar13.jpg" class="img-avatar img-avatar-thumb" alt="Default Avatar" />
+        </div>
+        <h1 class="h2 text-white mb-0">Edit Account</h1>
+        <h2 class="h4 fw-normal text-white-75">{{ authUser.name }}</h2>
+        <RouterLink :to="{ name: 'backend-pages-generic-profile' }" class="btn btn-alt-secondary">
+          <i class="fa fa-fw fa-arrow-left text-danger"></i> Back to Profile
+        </RouterLink>
+      </div>
+    </div>
+  </BaseBackground>
+  <!-- END Hero -->
+
+  <!-- Page Content -->
+  <div class="content content-boxed">
+    <!-- User Profile -->
+    <BaseBlock title="User Profile">
+      <div v-if="credentialDetailError" class="alert alert-danger" role="alert">
+        {{ credentialDetailErrorMessage }}
+      </div>
+      <form @submit.prevent="detailSubmit">
+        <div class="row push">
+          <div class="col-lg-4">
+            <p class="fs-sm text-muted">
+              Your account’s vital info.
+            </p>
+          </div>
+          <div class="col-lg-8 col-xl-5">
+            <div class="mb-4">
+              <label class="form-label" for="one-profile-edit-name">Name</label>
+              <input type="text" class="form-control" id="one-profile-edit-name" name="one-profile-edit-name"
+                placeholder="Enter your name.." v-model="detailState.name" :class="{
+                  'is-invalid': vDetailRules$.name.$errors.length,
+                }" @blur="vDetailRules$.name.$touch" />
+              <div v-if="vDetailRules$.name.$errors.length" class="invalid-feedback animated fadeIn">
+                <ul style="list-style: none" class="p-0">
+                  <li v-for="error in vDetailRules$.name.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="mb-4">
+              <label class="form-label" for="one-profile-edit-email">Email Address</label>
+              <input type="email" class="form-control" id="one-profile-edit-email" name="one-profile-edit-email"
+                placeholder="Enter your email.." v-model="detailState.email" :class="{
+                  'is-invalid': vDetailRules$.email.$errors.length,
+                }" @blur="vDetailRules$.email.$touch" />
+              <div v-if="vDetailRules$.email.$errors.length" class="invalid-feedback animated fadeIn">
+                <ul style="list-style: none" class="p-0">
+                  <li v-for="error in vDetailRules$.email.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <ImageUploader :userAvatar="authUser.avatar" @imageChanged="onImageChange"></ImageUploader>
+            <div class="mb-4">
+              <button type="submit" class="btn btn-alt-primary">Update</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </BaseBlock>
+    <!-- END User Profile -->
+
+    <!-- Change Password -->
+    <BaseBlock title="Change Password">
+      <div v-if="credentialPasswordError" class="alert alert-danger" role="alert">
+        {{ credentialPasswordErrorMessage }}
+      </div>
+      <form @submit.prevent="passwordSubmit">
+        <div class="row push">
+          <div class="col-lg-4">
+            <p class="fs-sm text-muted">
+              Changing your sign in password is an easy way to keep your account
+              secure.
+            </p>
+          </div>
+          <div class="col-lg-8 col-xl-5">
+            <div class="mb-4">
+              <label class="form-label" for="one-profile-edit-password">Current Password</label>
+              <input type="password" class="form-control" id="one-profile-edit-password" name="one-profile-edit-password"
+                v-model="passwordState.current_password" :class="{
+                  'is-invalid': vPasswordRules$.current_password.$errors.length,
+                }" @blur="vPasswordRules$.current_password.$touch" />
+              <div v-if="vPasswordRules$.current_password.$errors.length" class="invalid-feedback animated fadeIn">
+                <ul style="list-style: none" class="p-0">
+                  <li v-for="error in vPasswordRules$.current_password.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="row mb-4">
+              <div class="col-12">
+                <label class="form-label" for="one-profile-edit-password-new">New Password</label>
+                <input type="password" class="form-control" id="one-profile-edit-password-new"
+                  name="one-profile-edit-password-new" v-model="passwordState.password" :class="{
+                    'is-invalid': vPasswordRules$.password.$errors.length
+                  }" @blur="vPasswordRules$.password.$touch" />
+                <div v-if="vPasswordRules$.password.$errors.length" class="invalid-feedback animated fadeIn">
+                  <ul style="list-style: none" class="p-0">
+                    <li v-for="error in vPasswordRules$.password.$errors" :key="error.$uid">
+                      {{ error.$message }}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="row mb-4">
+              <div class="col-12">
+                <label class="form-label" for="one-profile-edit-password-new-confirm">Confirm New Password</label>
+                <input type="password" class="form-control" id="one-profile-edit-password-new-confirm"
+                  name="one-profile-edit-password-new-confirm" v-model="passwordState.password_confirmation" :class="{
+                    'is-invalid': vPasswordRules$.password_confirmation.$errors.length
+                  }"
+                  @blur="vPasswordRules$.password_confirmation.$touch" />
+                <div v-if="vPasswordRules$.password_confirmation.$errors.length" class="invalid-feedback animated fadeIn">
+                  <ul style="list-style: none" class="p-0">
+                    <li v-for="error in vPasswordRules$.password_confirmation.$errors" :key="error.$uid">
+                      {{ error.$message === 'The value must be equal to the other value' ?
+                        'The passwords do not match' :
+                        error.$message }}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <button type="submit" class="btn btn-alt-primary">Update</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </BaseBlock>
+    <!-- END Change Password -->
+  </div>
+  <!-- END Page Content -->
+</template>
+
 <script setup>
 import { ref, reactive, computed } from "vue";
 import { useRouter } from "vue-router";
@@ -91,17 +239,17 @@ const detailSubmit = () => {
             "Content-Type": "multipart/form-data", // Use multipart/form-data for file upload
           },
         })
-        .catch((error) => {
-          console.log({ error });
-          return;
-        });
+          .catch((error) => {
+            console.log({ error });
+            return;
+          });
       }
 
       axios.post(`/api/users/${route.params?.user.id}/update`, detailState, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
         .then((response) => {
           if (response.data?.type === 'success') {
             Swal.fire({
@@ -110,7 +258,7 @@ const detailSubmit = () => {
               showConfirmButton: false,
               timer: 1500,
             }).then(() => {
-              location.reload();
+              // location.reload();
             });
           }
         })
@@ -130,15 +278,15 @@ const passwordSubmit = () => {
         },
       }).then((response) => {
         if (response.data?.type === 'success') {
-            Swal.fire({
-              icon: 'success',
-              title: 'Password Updated Successfully!',
-              showConfirmButton: false,
-              timer: 1500,
-            }).then(() => {
-              router.push({ name: 'backend-pages-generic-profile' });
-            });
-          }
+          Swal.fire({
+            icon: 'success',
+            title: 'Password Updated Successfully!',
+            showConfirmButton: false,
+            timer: 1500,
+          }).then(() => {
+            router.push({ name: 'backend-pages-generic-profile' });
+          });
+        }
       }).catch((error) => {
         credentialPasswordError.value = true;
         credentialPasswordErrorMessage.value = 'There has been an error, please try again';
@@ -152,213 +300,3 @@ const onImageChange = (newImage) => {
   detailState.avatar = newImage;
 };
 </script>
-
-<template>
-<!-- Hero -->
-<BaseBackground image="/assets/media/photos/photo10@2x.jpg">
-    <div class="bg-primary-dark-op">
-      <div class="content content-full text-center">
-        <div class="my-3">
-          <img v-if="authUser.avatar !== ''" :src="authUser.avatar" class="img-avatar img-avatar-thumb" alt="User Avatar" />
-          <img v-else src="/assets/media/avatars/avatar13.jpg" class="img-avatar img-avatar-thumb" alt="Default Avatar" />
-        </div>
-        <h1 class="h2 text-white mb-0">Edit Account</h1>
-        <h2 class="h4 fw-normal text-white-75">{{ authUser.name }}</h2>
-        <RouterLink
-          :to="{ name: 'backend-pages-generic-profile' }"
-          class="btn btn-alt-secondary"
-        >
-          <i class="fa fa-fw fa-arrow-left text-danger"></i> Back to Profile
-        </RouterLink>
-      </div>
-    </div>
-  </BaseBackground>
-  <!-- END Hero -->
-
-  <!-- Page Content -->
-  <div class="content content-boxed">
-    <!-- User Profile -->
-    <BaseBlock title="User Profile">
-      <div v-if="credentialDetailError" class="alert alert-danger" role="alert">
-        {{ credentialDetailErrorMessage }}
-      </div>
-      <form @submit.prevent="detailSubmit">
-        <div class="row push">
-          <div class="col-lg-4">
-            <p class="fs-sm text-muted">
-              Your account’s vital info.
-            </p>
-          </div>
-          <div class="col-lg-8 col-xl-5">
-            <div class="mb-4">
-              <label class="form-label" for="one-profile-edit-name">Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="one-profile-edit-name"
-                name="one-profile-edit-name"
-                placeholder="Enter your name.."
-                v-model="detailState.name"
-                :class="{
-                  'is-invalid': vDetailRules$.name.$errors.length,
-                }"
-                @blur="vDetailRules$.name.$touch"
-              />
-              <div
-                  v-if="vDetailRules$.name.$errors.length"
-                  class="invalid-feedback animated fadeIn"
-                >
-                <ul style="list-style: none" class="p-0">
-                  <li v-for="error in vDetailRules$.name.$errors" :key="error.$uid">
-                    {{ error.$message }}
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="mb-4">
-              <label class="form-label" for="one-profile-edit-email"
-                >Email Address</label
-              >
-              <input
-                type="email"
-                class="form-control"
-                id="one-profile-edit-email"
-                name="one-profile-edit-email"
-                placeholder="Enter your email.."
-                v-model="detailState.email"
-                :class="{
-                  'is-invalid': vDetailRules$.email.$errors.length,
-                }"
-                @blur="vDetailRules$.email.$touch"
-              />
-              <div
-                  v-if="vDetailRules$.email.$errors.length"
-                  class="invalid-feedback animated fadeIn"
-                >
-                <ul style="list-style: none" class="p-0">
-                  <li v-for="error in vDetailRules$.email.$errors" :key="error.$uid">
-                    {{ error.$message }}
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <ImageUploader :userAvatar="authUser.avatar" @imageChanged="onImageChange"></ImageUploader>
-            <div class="mb-4">
-              <button type="submit" class="btn btn-alt-primary">Update</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </BaseBlock>
-    <!-- END User Profile -->
-
-    <!-- Change Password -->
-    <BaseBlock title="Change Password">
-      <div v-if="credentialPasswordError" class="alert alert-danger" role="alert">
-        {{ credentialPasswordErrorMessage }}
-      </div>
-      <form @submit.prevent="passwordSubmit">
-        <div class="row push">
-          <div class="col-lg-4">
-            <p class="fs-sm text-muted">
-              Changing your sign in password is an easy way to keep your account
-              secure.
-            </p>
-          </div>
-          <div class="col-lg-8 col-xl-5">
-            <div class="mb-4">
-              <label class="form-label" for="one-profile-edit-password"
-                >Current Password</label
-              >
-              <input
-                type="password"
-                class="form-control"
-                id="one-profile-edit-password"
-                name="one-profile-edit-password"
-                v-model="passwordState.current_password"
-                :class="{
-                  'is-invalid': vPasswordRules$.current_password.$errors.length,
-                }"
-                @blur="vPasswordRules$.current_password.$touch"
-              />
-              <div
-                  v-if="vPasswordRules$.current_password.$errors.length"
-                  class="invalid-feedback animated fadeIn"
-                >
-                <ul style="list-style: none" class="p-0">
-                  <li v-for="error in vPasswordRules$.current_password.$errors" :key="error.$uid">
-                    {{ error.$message }}
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="row mb-4">
-              <div class="col-12">
-                <label class="form-label" for="one-profile-edit-password-new"
-                  >New Password</label
-                >
-                <input
-                  type="password"
-                  class="form-control"
-                  id="one-profile-edit-password-new"
-                  name="one-profile-edit-password-new"
-                  v-model="passwordState.password"
-                  :class="{
-                  'is-invalid': vPasswordRules$.password.$errors.length}"
-                  @blur="vPasswordRules$.password.$touch"
-                />
-                <div
-                    v-if="vPasswordRules$.password.$errors.length"
-                    class="invalid-feedback animated fadeIn"
-                  >
-                  <ul style="list-style: none" class="p-0">
-                    <li v-for="error in vPasswordRules$.password.$errors" :key="error.$uid">
-                      {{ error.$message }}
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="row mb-4">
-              <div class="col-12">
-                <label
-                  class="form-label"
-                  for="one-profile-edit-password-new-confirm"
-                  >Confirm New Password</label
-                >
-                <input
-                  type="password"
-                  class="form-control"
-                  id="one-profile-edit-password-new-confirm"
-                  name="one-profile-edit-password-new-confirm"
-                  v-model="passwordState.password_confirmation"
-                  :class="{
-                  'is-invalid': vPasswordRules$.password_confirmation.$errors.length}"
-                  @blur="vPasswordRules$.password_confirmation.$touch"
-                />
-                <div
-                    v-if="vPasswordRules$.password_confirmation.$errors.length"
-                    class="invalid-feedback animated fadeIn"
-                  >
-                  <ul style="list-style: none" class="p-0">
-                    <li v-for="error in vPasswordRules$.password_confirmation.$errors" :key="error.$uid">
-                      {{ error.$message === 'The value must be equal to the other value' ? 
-                      'The passwords do not match' : 
-                      error.$message }}
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <button type="submit" class="btn btn-alt-primary">Update</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </BaseBlock>
-    <!-- END Change Password -->
-  </div>
-  <!-- END Page Content -->
-
-</template>
