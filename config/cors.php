6f53c228e149d5a5e5c39dcc6cc97e16f34e7c4e
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2023-11-23 13:02:27 +0000

    Changed URL in cors file

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://nexusflo-136651299275.herokuapp.com/'],
+    'allowed_origins' => ['https://nexusflo.uk/'],
 
     'allowed_origins_patterns' => [],
 
