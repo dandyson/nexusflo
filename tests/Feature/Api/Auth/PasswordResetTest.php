45660202a0633e9308f93586053d57bbb7996647
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-08-27 14:34:41 +0100

    🚨 Lint fixes for test

--- tests/Feature/Api/Auth/PasswordResetTest.php
+++ tests/Feature/Api/Auth/PasswordResetTest.php
@@ -3,9 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Hash;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
