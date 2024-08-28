a034144ff4cb91ac3056e073fb9a7afad7abd8d3
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-08-28 17:38:48 +0100

    Removed dd and adding cache clearing to pipeline

--- tests/Feature/AiControllerTest.php
+++ tests/Feature/AiControllerTest.php
@@ -27,8 +27,6 @@ class AiControllerTest extends TestCase
 
         // Replace the real client with the fake client only for this test
         $this->app->instance(Client::class, $fakeClient);
-
-        dd($fakeClient);
     }
 
     /**
