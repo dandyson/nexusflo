8aa75506d3f13b12273c6eaf806945eefd709499
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2025-07-30 15:47:35 +0100

    💥 Removed registerPolicies as it is now invoked automatically by the framework

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,10 +25,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        //
-
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return new VerifyMail($notifiable, $url);
         });
